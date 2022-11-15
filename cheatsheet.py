aee62578dc0e106afb59343f7f93b0adb60733be
River <riveriswild@icloud.com>
2022-11-15 16:12:54 +0300

    Tuple info added

--- cheatsheet.py
+++ cheatsheet.py
@@ -156,3 +156,9 @@ set.union(anotherSet) # ...
 set.update(anotherSet) # adds anotherSet without duplicate
 
 # tuple
+tuple = (1,2,3,1)
+
+tuple.count(1) # returns occurence of an item
+tuple.index(1) # returns index of 1 in array
+
+
